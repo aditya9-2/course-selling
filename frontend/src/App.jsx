15dc350e9a12a65050f944d443e0da13c946df7d
Aditya <adityabasak7031@gmail.com>
2024-12-14 22:19:59 +0530

    add routing

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,19 @@
 import Home from "./pages/Home";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Registration from "./pages/Registration";
+import Login from "./pages/Login";
 
 function App() {
-  return (
-    <div>
-      <Home />
-    </div>
+  const routes = (
+    <Router>
+      <Routes>
+        <Route path="/user/registration" exact element={<Registration />} />
+        <Route path="/user/login" exact element={<Login />} />
+        <Route path="/user/home" exact element={<Home />} />
+      </Routes>
+    </Router>
   );
+  return <>{routes}</>;
 }
 
 export default App;
