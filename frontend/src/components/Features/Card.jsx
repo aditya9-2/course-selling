43b7b977f84897caf043d24bdab9b0ee7946db02
Aditya <adityabasak7031@gmail.com>
2024-12-15 23:24:31 +0530

    m

--- frontend/src/components/Features/Card.jsx
+++ frontend/src/components/Features/Card.jsx
@@ -4,21 +4,21 @@ const Card = ({ cardImage, title, price }) => {
   const handlesShowCourseDetails = () => {};
 
   return (
-    <div className="border rounded-3xl shadow-md bg-white   mt-9 md:mt-2 border-zinc-400 w-[22rem] ">
+    <div className="border rounded-3xl shadow-md bg-white mt-9 md:mt-2 border-zinc-400 w-[22rem] cursor-pointer">
       <img
         src={cardImage}
         alt="CardImage"
         className="w-full h-1/2 object-cover rounded-t-3xl"
       />
 
-      <div className="p-2">
+      <div className="p-2 ">
         <h3 className="text-lg text-black font-extrabold">{title}</h3>
         <p className="text-black  font-bold mt-10 mb-4">
           {" "}
           <span>&#8377;</span>
           {price}
         </p>
-        <div className=" mb-4">
+        <div>
           <PrimaryBtn
             type={"View Details"}
             onclick={handlesShowCourseDetails}
