9638634f8395ee0fdf3b53926bd07b297a8c287e
Aditya <adityabasak7031@gmail.com>
2025-02-17 20:59:30 +0530

    add forwardRef

--- frontend/src/components/PasswordInput.jsx
+++ frontend/src/components/PasswordInput.jsx
@@ -1,8 +1,9 @@
 /* eslint-disable react/prop-types */
 import { EyeClosed, Eye } from "lucide-react";
-import { useState } from "react";
+import { forwardRef, useState } from "react";
 
-const PasswordInput = ({ placeholder }) => {
+const PasswordInput = forwardRef(({ placeholder }, ref) => {
+  // `ref` should be the first argument
   const [showPassword, setShowPassword] = useState(false);
 
   const toggleShowPassword = () => {
@@ -12,13 +13,13 @@ const PasswordInput = ({ placeholder }) => {
   return (
     <div className="relative w-full">
       <input
+        ref={ref}
         type={showPassword ? "text" : "password"}
         placeholder={placeholder}
         required
         className="w-full h-7 md:h-10 rounded p-3 pr-10 border border-slate-50 text-white text-sm outline-none bg-transparent"
       />
 
-      {/* Eye Icon Positioned Absolutely */}
       <div
         className="absolute inset-y-0 right-3 flex items-center cursor-pointer"
         onClick={toggleShowPassword}
@@ -31,6 +32,8 @@ const PasswordInput = ({ placeholder }) => {
       </div>
     </div>
   );
-};
+});
+
+PasswordInput.displayName = "PasswordInput";
 
 export default PasswordInput;
