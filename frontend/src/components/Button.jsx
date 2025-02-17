28805d0f63fba3628b42f02d0eb8951aa69093ed
Aditya <adityabasak7031@gmail.com>
2025-02-17 22:01:57 +0530

    fix typo

--- frontend/src/components/Button.jsx
+++ frontend/src/components/Button.jsx
@@ -1,8 +1,8 @@
 /* eslint-disable react/prop-types */
-const Button = ({ type, label, onclick }) => {
+const Button = ({ type, label, onClick }) => {
   return (
     <div
-      onClick={onclick}
+      onClick={onClick}
       className={`px-6 py-1 md:py-2 rounded cursor-pointer hover:opacity-90 text-center
          ${
            type === "border-btn" ? "border border-white text-white" : "bg-white"
