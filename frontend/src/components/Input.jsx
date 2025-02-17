f2a41f5dbe46d1c625e15beddd1f3efd3ff21286
Aditya <adityabasak7031@gmail.com>
2025-02-17 20:59:14 +0530

    ass forwardRef

--- frontend/src/components/Input.jsx
+++ frontend/src/components/Input.jsx
@@ -1,15 +1,18 @@
+import { forwardRef } from "react";
+
 /* eslint-disable react/prop-types */
-const Input = ({ type, placeholder }) => {
+const Input = forwardRef(({ type, placeholder }, ref) => {
   return (
-    <>
-      <input
-        type={type}
-        placeholder={placeholder}
-        required
-        className="h-7 md:h-10 rounded p-2 border border-slate-50 text-white text-sm outline-0"
-      />
-    </>
+    <input
+      ref={ref}
+      type={type}
+      placeholder={placeholder}
+      required
+      className="h-7 md:h-10 rounded p-2 border border-slate-50 text-white text-sm outline-0"
+    />
   );
-};
+});
+
+Input.displayName = "Input";
 
 export default Input;
