13f9dea1ea7c2b366bf0ab3567486ef2ea905859
Aditya <adityabasak7031@gmail.com>
2025-02-17 19:39:33 +0530

    fix text-alignment

--- frontend/src/components/Button.jsx
+++ frontend/src/components/Button.jsx
@@ -3,7 +3,7 @@ const Button = ({ type, label, onclick }) => {
   return (
     <div
       onClick={onclick}
-      className={`px-6 py-2 rounded cursor-pointer hover:opacity-90 
+      className={`px-6 py-1 md:py-2 rounded cursor-pointer hover:opacity-90 text-center
          ${
            type === "border-btn" ? "border border-white text-white" : "bg-white"
          }`}
