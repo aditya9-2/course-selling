26fa772af61c1428e87251d38e0b46c7860dfed7
Aditya <adityabasak7031@gmail.com>
2025-02-18 15:53:39 +0530

    add width prop

--- frontend/src/components/Button.jsx
+++ frontend/src/components/Button.jsx
@@ -1,9 +1,10 @@
 /* eslint-disable react/prop-types */
-const Button = ({ type, label, onClick }) => {
+const Button = ({ type, label, onClick, width = "w-auto" }) => {
   return (
     <div
       onClick={onClick}
       className={`px-6 py-1 md:py-2 rounded cursor-pointer hover:opacity-90 text-center
+        ${width}
          ${
            type === "border-btn" ? "border border-white text-white" : "bg-white"
          }`}
