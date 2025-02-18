4ab30143dbaff6e73d8047af15e0ec6ddd32effd
Aditya <adityabasak7031@gmail.com>
2025-02-18 17:13:50 +0530

    add onClick prop

--- frontend/src/components/Hero.jsx
+++ frontend/src/components/Hero.jsx
@@ -8,10 +8,11 @@ const Hero = ({
   color = "bg-[#5A47AB]",
   txtClr = "text-[#FBD15B]",
   subTxtColor = "text-[#F2F2F2]",
+  onClick,
 }) => {
   return (
-    <div className={`h-75 md:h-[25.5rem] ${color} py-4 px-4 md:px-14`}>
-      <div className="flex justify-between items-center  gap-5 ">
+    <div className={`h-auto md:h-[25.5rem] ${color} py-4 px-4 md:px-14`}>
+      <div className="flex flex-col-reverse md:flex-row md:justify-between items-center  gap-5 ">
         <div
           className={`${txtClr} text-wrap md:text-nowrap leading-6 md:leading-0`}
         >
@@ -25,12 +26,12 @@ const Hero = ({
             {sublabel}
           </p>
         </div>
-        <div className="w-96 md:w-84">
+        <div className="w-65 md:w-96">
           <img src={image} alt="" />
         </div>
       </div>
       <div className="mt-10 md:mt-0 relative md:bottom-10">
-        <Button label={"Explore now"} width={"w-34 md:w-54"} />
+        <Button label={"Explore now"} width={"md:w-54"} onClick={onClick} />
       </div>
     </div>
   );
