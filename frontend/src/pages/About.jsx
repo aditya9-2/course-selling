3b04f86ccbd99d528427d12ec040718a7eaa8a68
Aditya <adityabasak7031@gmail.com>
2025-02-18 17:24:41 +0530

    add about page

--- frontend/src/pages/About.jsx
+++ frontend/src/pages/About.jsx
@@ -1,5 +1,51 @@
 const About = () => {
-  return <div>About</div>;
+  return (
+    <div className="h-auto p-18">
+      <h1 className="text-4xl py-4 font-cursive text-[#39229A] font-semibold">
+        About The Company
+      </h1>
+      <p className="text-justify">
+        {" "}
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora ut
+        suscipit, neque ad, impedit voluptatem ipsam aliquam iure doloremque
+        maxime vero dolore est adipisci veritatis blanditiis illo ab. Modi
+        dolor, vel nostrum quaerat asperiores earum natus quae explicabo quod ab
+        dicta beatae amet sed alias necessitatibus cupiditate, dolore
+        consectetur delectus eligendi!
+      </p>
+      <h1 className="text-4xl py-4 font-cursive text-[#39229A] font-semibold">
+        About The Developer
+      </h1>
+      <p className="text-justify">
+        {" "}
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora ut
+        suscipit, neque ad, impedit voluptatem ipsam aliquam iure doloremque
+        maxime vero dolore est adipisci veritatis blanditiis illo ab. Modi
+        dolor, vel nostrum quaerat asperiores earum natus quae explicabo quod ab
+        dicta beatae amet sed alias necessitatibus cupiditate, dolore
+        consectetur delectus eligendi!
+      </p>
+
+      <h1 className="text-4xl py-4 font-cursive text-[#39229A] font-semibold">
+        Vision & Mission
+      </h1>
+      <p className="text-justify">
+        {" "}
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora ut
+        suscipit, neque ad, impedit voluptatem ipsam aliquam iure doloremque
+        maxime vero dolore est adipisci veritatis blanditiis illo ab. Modi
+        dolor, vel nostrum quaerat asperiores earum natus quae explicabo quod ab
+        dicta beatae amet sed alias necessitatibus cupiditate, dolore
+        consectetur delectus eligendi!
+      </p>
+      <p className="text-center mt-10 text-xl">
+        Hi, I am <span className="text-purple-700">Aditya</span>
+        <br />
+        and I am The Developer{" "}
+        <span className="font-cursive font-semibold text-[#FBD15B]">Bitch</span>
+      </p>
+    </div>
+  );
 };
 
 export default About;
