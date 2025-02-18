3fcd9c7d45be0969f6deb3af3c1e23e3992baaf9
Aditya <adityabasak7031@gmail.com>
2025-02-19 00:10:53 +0530

    add all courses Dynamicly

--- frontend/src/pages/Courses.jsx
+++ frontend/src/pages/Courses.jsx
@@ -1,5 +1,60 @@
+import CourseCard from "../components/CourseCard";
+import { useEffect, useState } from "react";
+import axios from "axios";
+
 const Courses = () => {
-  return <div>Courses</div>;
+  const [courses, setCourses] = useState([]);
+
+  const fetchCourse = async () => {
+    try {
+      const response = await axios.get(
+        `http://localhost:3000/api/v1/course/preview-Courses`
+      );
+      const data = response.data;
+      console.log(JSON.stringify(data));
+
+      if (data.courses && Array.isArray(data.courses)) {
+        setCourses(data.courses);
+      } else {
+        setCourses([]);
+      }
+    } catch (err) {
+      console.log(`Error While Finding the course: ${err.message}`);
+    }
+  };
+
+  useEffect(() => {
+    fetchCourse();
+  }, []);
+
+  return (
+    <div className="h-auto py-18">
+      <h1 className="text-3xl md:text-4xl mt-3 text-center font-semibold">
+        All Available <span className="text-[#39229A]">Courses</span>
+      </h1>
+
+      {courses.length === 0 ? (
+        <div className="h-screen flex justify-center mt-20">
+          <p className="text-center text-3xl mt-4">No courses available.</p>
+        </div>
+      ) : (
+        <div className="flex flex-wrap justify-center items-center gap-10 mt-4">
+          {courses.map((course) => (
+            <CourseCard
+              key={course._id}
+              onClick={() => console.log(`Clicked ${course.title}`)}
+              img={course.imageUrl} // Use course image
+              title={course.title}
+              subtitle={course.description}
+              price={course.price}
+            />
+          ))}
+        </div>
+      )}
+
+      <div className="flex flex-wrap justify-center items-center gap-10 mt-4"></div>
+    </div>
+  );
 };
 
 export default Courses;
