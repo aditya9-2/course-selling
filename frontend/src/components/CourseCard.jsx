349bd7a783b9c929b5b3e50e4aa065a9575c5df0
Aditya <adityabasak7031@gmail.com>
2025-02-19 00:13:04 +0530

    fix alignment of card

---
+++ frontend/src/components/CourseCard.jsx
@@ -0,0 +1,25 @@
+/* eslint-disable react/prop-types */
+
+import SecondaryBtn from "./secondaryBtn";
+const CourseCard = ({ title, subtitle, price, img, onCLick }) => {
+  return (
+    <div className="bg-white h-84 min-h-[400px] w-80  rounded shadow-xl border border-gray-600 flex flex-col">
+      <img src={img} alt="" className="w-full h-40 object-cover rounded-t" />
+
+      <h3 className="text-xl font-semibold mt-2 text-center">{title}</h3>
+
+      <p className="text-sm text-gray-600 mt-2 px-5">
+        {subtitle.length > 120 ? `${subtitle.slice(0, 120)}...` : subtitle}
+      </p>
+      <p className="text-xl font-bold px-5 pt-4 text-[#39229A] mt-2">
+        ₹{price}
+      </p>
+
+      <div className="px-4 mt-auto py-4 flex justify-center">
+        <SecondaryBtn label={"View Course"} onCLick={onCLick} />
+      </div>
+    </div>
+  );
+};
+
+export default CourseCard;
