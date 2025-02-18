f122d0ca364f91b42741ad75432120404e3a43c8
Aditya <adityabasak7031@gmail.com>
2025-02-18 18:56:24 +0530

    remove toast

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -2,7 +2,7 @@ import { GraduationCap, ArrowRightFromLine, AlignRight } from "lucide-react";
 import Button from "./Button";
 import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
-import { toast } from "sonner";
+
 import useAuthStore from "../store/authStore";
 
 const Navbar = () => {
