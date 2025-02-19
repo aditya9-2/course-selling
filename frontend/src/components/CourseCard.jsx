156c1600559bb7ad15c2f2dd8d7b198ded6ffa0f
Aditya <adityabasak7031@gmail.com>
2025-02-19 21:06:09 +0530

    change subtitile length

--- frontend/src/components/CourseCard.jsx
+++ frontend/src/components/CourseCard.jsx
@@ -9,7 +9,7 @@ const CourseCard = ({ title, subtitle, price, img, onClick, BtnLabel }) => {
       <h3 className="text-xl font-semibold mt-2 text-center">{title}</h3>
 
       <p className="text-sm text-gray-600 mt-2 px-5">
-        {subtitle.length > 120 ? `${subtitle.slice(0, 120)}...` : subtitle}
+        {subtitle.length > 80 ? `${subtitle.slice(0, 80)}...` : subtitle}
       </p>
       <p className="text-xl font-bold px-5 pt-4 text-[#39229A] mt-2">
         ₹{price}
