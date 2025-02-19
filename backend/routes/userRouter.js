3efd9a5ed56b8ff75aa98d2595af99c33dea14f0
Aditya <adityabasak7031@gmail.com>
2025-02-19 19:39:09 +0530

    rm userPurchase - empty

--- backend/routes/userRouter.js
+++ backend/routes/userRouter.js
@@ -1,15 +1,14 @@
 import express from "express";
 import userRegistration from "../controllers/user/userRegistrationController.js";
 import userLogin from "../controllers/user/userLoginController.js";
-import getUserPurchases from "../controllers/user/getUserPurchases.js";
-import userauthentication from "../middleware/userAuthTokenMiddleware.js";
+
 
 const router = express.Router();
 
 
 router.post('/user-registration', userRegistration);
 router.post('/user-login', userLogin);
-router.get('/user-purchases', userauthentication, getUserPurchases);
+
 
 
 export default router;
