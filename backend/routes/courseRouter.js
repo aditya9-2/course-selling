3f2fe4ad0169003f398765b7866120a5372cb679
Aditya <adityabasak7031@gmail.com>
2025-02-19 22:06:03 +0530

    remove payment router from courseRouter

--- backend/routes/courseRouter.js
+++ backend/routes/courseRouter.js
@@ -1,12 +1,11 @@
 import express from "express";
 
-import purchaseCourse from "../controllers/course/purchaseCourseController.js";
+
 import userauthentication from "../middleware/userAuthTokenMiddleware.js";
 import adminAuthentication from "../middleware/adminAuthenticationToken.js"
 import getAllAdminCourses from "../controllers/course/getAllAdminCourseController.js";
 import getAlluserCourses from "../controllers/course/getAllUserscoursesController.js";
 import previewCourses from "../controllers/course/previewCoursesController..js";
-import confirmPayment from "../controllers/course/confirmPaymentController.js";
 import getCourseDetails from "../controllers/course/getCourseDetailsController.js";
 
 
@@ -16,8 +15,7 @@ router.get('/preview-Courses', previewCourses);
 router.get('/get-course-details/:id', getCourseDetails);
 router.get('/all-user-courses', userauthentication, getAlluserCourses);
 router.get('/all-admin-courses', adminAuthentication, getAllAdminCourses);
-router.post('/purchase-course', userauthentication, purchaseCourse);
-router.post('/confiirm-payment', userauthentication, confirmPayment);
+
 
 
 
